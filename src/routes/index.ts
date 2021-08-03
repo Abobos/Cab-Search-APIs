765ddc6100d5e248af5c28af34c07ae13f1fab99
Blessing Makaraba <blessingmakaraba@Blessings-MacBook-Pro.local>
2021-08-03 15:08:36 +0100

    Finish API Endpoints

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,6 @@
-import { Router, Request, Response } from "express";
+import { Router, Request, Response, response } from "express";
+
+import https from "https";
 
 import driverRoute from "./driver";
 
@@ -19,6 +21,45 @@ router.get("/", (req: Request, res: Response) =>
     200,
     "Welcome to Cabs Searh Application System API"
   )
+);
+
+router.get(
+  "/justeat",
+  (req: Request, res: Response) => {
+    console.log("I'm here");
+
+    const https = require("https");
+
+    https
+      .get(
+        "https://uk.api.just-eat.io/restaurants/bypostcode/ec4m",
+        (resp: any) => {
+          let data = "";
+
+          // A chunk of data has been received.
+          resp.on("data", (chunk: any) => {
+            data += chunk;
+          });
+
+          // The whole response has been received. Print out the result.
+          resp.on("end", () => {
+            res.status(200).json({ status: "success", data: JSON.parse(data) });
+          });
+        }
+      )
+      .on("error", (err: any) => {
+        console.log("Error: " + err.message);
+        res
+          .status(500)
+          .json({ status: "fail", message: "Something went wrong" });
+      });
+  }
+
+  // sendSuccessResponseII(
+  //   res,
+  //   200,
+  //   "Welcome to Cabs Searh Application System API"
+  // )
 );
 
 router.use("/api/v1", [driverRoute, locationRoute, carRoute]);
